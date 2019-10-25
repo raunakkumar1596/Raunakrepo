d06811750eaeeba33a459e3053b90672cdcd08aa
Raunak Kumar <raunakkiit2015@gmail.com>
2019-10-25 11:02:05 +0530

    File Added

---
+++ DevOps/DevOps/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Capgemini Module Test");
+            Console.ReadKey();
+        }
+    }
+}
